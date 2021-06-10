e25f0931880bbb110a5d282b64c97c8c21ee891d
surafelaron <surafelaron@gmail.com>
2021-06-09 18:12:51 -0700

    set mongodb with atlas

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,14 +11,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/recycle",
-    {
-      useCreateIndex: true,
-      useNewUrlParser: true
-    }
-  );
-
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true}, ()=>{
+  console.log("db connected")
+})
 
 app.listen(PORT, function(){
     console.log( ` 🌎  ==>API server now listening on PORT ${PORT}`)
